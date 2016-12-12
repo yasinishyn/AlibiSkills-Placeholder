49503828f5c407e7e547d744289da0e06bf61cfa
iryna-kovalchuk <irene.kovaltchuk@gmail.com>
2016-12-12 18:40:47 +0200

    add success handler for form submitting

--- js/local.js
+++ js/local.js
@@ -10,20 +10,32 @@ $(document).ready(function() {
       $("[name='entry.340286705']").val('teacher');
   });
 
+  var successHandler = function() {
+    var form = $('.user-info')
+    form.find('h4').text('Alibi Skills никогда не забывает тех, кто не равнодушен к проекту, а Вы уже помогаете его развитию. Спасибо Вам за доверие.');
+    form.find('input:not(:hidden)').each(function(){
+      $(this).hide();
+      $('#i_student, #i_teacher').off('click');
+    });
+  }
+
   $('.user-info').submit(function(e) {
     e.preventDefault();
-    var form = $(this);
-
-    var data = form.serialize();
+    var data = $(this).serialize();
     var baseURL = 'https://docs.google.com/forms/d/e/1FAIpQLScNdo-dx4tDXom1aw6gV85-sa5yXZTQN9rkQeZRLdHzC0EOJw/formResponse?';
     var submitRef = '&submit=7658006794998780417';
     var submitURL = (baseURL + data + submitRef);
-    $('.user-info h4').text('Alibi Skills никогда не забывает тех, кто не равнодушен к проекту, а Вы уже помогаете его развитию. Спасибо Вам за доверие.');
+
     $.ajax({
       url: submitURL,
       type: "POST",
-      dataType: "xml"
+      dataType: "xml",
+      statusCode: {
+        0: successHandler,
+        200: successHandler
+    }
     });
   });
+  return false;
 
 });
